d3049d5757635a1c502a08c52c54396ffddf730c
MarcelSunyer <45765502+MarcelSunyer@users.noreply.github.com>
2025-07-15 18:57:49 +0200

    Rotation ship

---
+++ Rockety Rocket 2/Assets/ShipController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+public class ShipController : MonoBehaviour
+{
+    public Rigidbody2D rigidbody2D;
+    public float moveSpeed = 5f;
+    float horizontalMovement;
+    float verticalMovement;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        rigidbody2D.rotation += horizontalMovement;
+    }
+
+    public void Move(InputAction.CallbackContext context)
+    {
+        horizontalMovement = context.ReadValue<Vector2>().x;
+    }
+}
