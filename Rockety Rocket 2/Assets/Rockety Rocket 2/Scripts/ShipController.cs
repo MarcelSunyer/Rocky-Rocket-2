0566d7afbda6d90b910bea67f9b506b0c73ba968
MarcelSunyer <45765502+MarcelSunyer@users.noreply.github.com>
2025-07-16 00:42:15 +0200

    Ship done

---
+++ Rockety Rocket 2/Assets/Rockety Rocket 2/Scripts/ShipController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class ShipController : MonoBehaviour
+{
+    public Rigidbody2D rigidbody2D;
+    public float rotationSpeed = 200f;
+    public float boostForce = 5f;
+
+    private float rotationInput;
+    private float boostInput;
+
+    void FixedUpdate()
+    {
+        // Rotación
+        rigidbody2D.rotation -= rotationInput * rotationSpeed * Time.fixedDeltaTime;
+
+        // Boost hacia donde apunta el ship
+        Vector2 direction = transform.up;
+        rigidbody2D.AddForce(direction * boostInput * (boostForce / 1000));
+    }
+
+    public void Rotation(InputAction.CallbackContext context)
+    {
+        rotationInput = context.ReadValue<Vector2>().x;
+    }
+
+    public void Boost(InputAction.CallbackContext context)
+    {
+        boostInput = context.ReadValue<Vector2>().y;
+    }
+}
