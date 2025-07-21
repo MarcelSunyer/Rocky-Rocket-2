8fa608717f2287d3f13df474d1a9c7bec72e648b
MarcelSunyer <45765502+MarcelSunyer@users.noreply.github.com>
2025-07-21 23:34:03 +0200

    Menu Manager + twening god

--- Rockety Rocket 2/Assets/Rockety Rocket 2/Scripts/MenuManager.cs
+++ Rockety Rocket 2/Assets/Rockety Rocket 2/Scripts/MenuManager.cs
@@ -1,23 +1,50 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEditor.SceneManagement;
-using UnityEngine.InputSystem;
 using UnityEngine.SceneManagement;
+using DG.Tweening;
+using UnityEngine.UI;
 public class MenuManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
+    public GameObject canvasFather;
 
-    }
+    public Button button_1;
+    public Button button_2;
+
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.H))
-        {
-            SceneManager.LoadScene("Main Scene");
-        }
+
+    }
+
+    public void StartButton()
+    {
+        canvasFather.transform.DOMoveY(-575, 1.34f,true);
+        button_2.Select();
+
+    }
+    public void SkinButton()
+    {
+        canvasFather.transform.DOMoveX(1050, 1.34f, false);
+        button_1.Select();
+    }
+    public void SkinToMain()
+    {
+        canvasFather.transform.DOMoveX(401.5f, 1.34f, false);
+
+    }
+    public void SettingsButton()
+    {
+        canvasFather.transform.DOMoveX(-525, 1.34f, true);
+    }
+
+    public void SelectedMap()
+    {
+        SceneManager.LoadScene("Main Scene");
+    }
+    public void Quit()
+    {
+        Application.Quit();
     }
 }
