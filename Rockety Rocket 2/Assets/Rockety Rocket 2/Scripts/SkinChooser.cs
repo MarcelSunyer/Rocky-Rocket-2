291b868d04720b70b0a67b35169d0547eacc3ba4
MarcelSunyer <45765502+MarcelSunyer@users.noreply.github.com>
2025-07-24 18:50:09 +0200

    Skins Chooser goood

--- Rockety Rocket 2/Assets/Rockety Rocket 2/Scripts/SkinChooser.cs
+++ Rockety Rocket 2/Assets/Rockety Rocket 2/Scripts/SkinChooser.cs
@@ -10,6 +10,10 @@ public class SkinChooser : MonoBehaviour
     private GameObject centerSkin;
     private GameObject rightSkin;
 
+    private int leftIndex = 0;
+    private int centerIndex = 1;
+    private int rightIndex = 2;
+
     public Vector3 positionLeft;
     public Vector3 positionCenter;
     public Vector3 positionRight;
@@ -34,15 +38,55 @@ public class SkinChooser : MonoBehaviour
     }
     public void MoveQueueRight()
     {
-        for (int i = 0; i < skins.Length; i++)
+        rightIndex = centerIndex;
+        centerIndex = leftIndex;
+        leftIndex = leftIndex - 1;
+
+        if(leftIndex < 0) 
         {
-            if (skins[i].isVisible)
-            {
-                print("Josemaria");
-            }
-        
-        
-        
+            leftIndex = 5;
         }
+
+        leftSkin = skins[leftIndex].gameObject;
+        leftSkin.transform.localPosition = positionLeft;
+        leftSkin.gameObject.SetActive(true);
+
+        centerSkin = skins[centerIndex].gameObject;
+        centerSkin.transform.localScale *= 1.5f;
+        centerSkin.transform.localPosition = positionCenter;
+
+        rightSkin.gameObject.SetActive(false);
+        rightSkin = skins[rightIndex].gameObject;
+        rightSkin.transform.localPosition = positionRight;
+        rightSkin.transform.localScale /= 1.5f;
+
+    }
+
+    public void MoveQueueLeft()
+    {
+        leftIndex = centerIndex;
+        centerIndex = rightIndex;
+        rightIndex = rightIndex + 1;
+
+        if (rightIndex > 5)
+        {
+            rightIndex = 0;
+        }
+
+        leftSkin.gameObject.SetActive(false);
+        leftSkin = skins[leftIndex].gameObject;
+        leftSkin.transform.localPosition = positionLeft;
+        leftSkin.transform.localScale /= 1.5f;
+
+
+        centerSkin = skins[centerIndex].gameObject;
+        centerSkin.transform.localScale *= 1.5f;
+        centerSkin.transform.localPosition = positionCenter;
+
+
+        rightSkin = skins[rightIndex].gameObject;
+        rightSkin.transform.localPosition = positionRight;
+        rightSkin.gameObject.SetActive(true);
+
     }
 }
