1da14774c2299e15baf34f9ef2ff29efcd46a793
MarcelSunyer <45765502+MarcelSunyer@users.noreply.github.com>
2025-07-24 17:02:46 +0200

    Chose Skin Wip, updare sprite UI

---
+++ Rockety Rocket 2/Assets/Rockety Rocket 2/Scripts/SkinChooser.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SkinChooser : MonoBehaviour
+{
+    public SpriteRenderer[] skins = new SpriteRenderer[5];
+
+    private GameObject leftSkin;
+    private GameObject centerSkin;
+    private GameObject rightSkin;
+
+    public Vector3 positionLeft;
+    public Vector3 positionCenter;
+    public Vector3 positionRight;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        leftSkin = skins[0].gameObject;
+        leftSkin.transform.localPosition = positionLeft;
+
+        centerSkin = skins[1].gameObject;
+        centerSkin.transform.localScale *= 1.5f;
+        centerSkin.transform.localPosition = positionCenter;
+
+        rightSkin = skins[2].gameObject;
+        rightSkin.transform.localPosition = positionRight;
+
+        skins[3].gameObject.SetActive(false);
+        skins[4].gameObject.SetActive(false);
+        skins[5].gameObject.SetActive(false);
+    }
+    public void MoveQueueRight()
+    {
+        for (int i = 0; i < skins.Length; i++)
+        {
+            if (skins[i].isVisible)
+            {
+                print("Josemaria");
+            }
+        
+        
+        
+        }
+    }
+}
