7969887d9a9f8d7fb51b9537b56c8441c4faa9cc
MarcelSunyer <45765502+MarcelSunyer@users.noreply.github.com>
2025-07-26 17:23:42 +0200

    Liada mesopotamica ab le esquins

--- Rockety Rocket 2/Assets/Rockety Rocket 2/Scripts/SkinChooser.cs
+++ Rockety Rocket 2/Assets/Rockety Rocket 2/Scripts/SkinChooser.cs
@@ -7,17 +7,32 @@ public class SkinChooser : MonoBehaviour
 {
     public SpriteRenderer[] skins = new SpriteRenderer[5];
 
+    private GameObject l_leftSkin;
     private GameObject leftSkin;
+
     private GameObject centerSkin;
+    private GameObject c_centerSkin;
+
     private GameObject rightSkin;
+    private GameObject r_rightSkin;
 
     private int leftIndex = 0;
     private int centerIndex = 1;
     private int rightIndex = 2;
 
+    public Vector3 position_L_Left;
     public Vector3 positionLeft;
+
     public Vector3 positionCenter;
+    public Vector3 position_C_Center;
+
     public Vector3 positionRight;
+    public Vector3 position_R_Right;
+
+
+    int safeIndexRight = 5;
+    int safeIndexCenter = 0;
+    int safeIndexLeft;
 
 
     // Start is called before the first frame update
@@ -33,9 +48,18 @@ public class SkinChooser : MonoBehaviour
         rightSkin = skins[2].gameObject;
         rightSkin.transform.DOLocalMove(positionRight, 0.4f);
 
-        skins[3].gameObject.SetActive(false);
-        skins[4].gameObject.SetActive(false);
-        skins[5].gameObject.SetActive(false);
+        r_rightSkin = skins[3].gameObject;
+        r_rightSkin.transform.DOLocalMove(position_R_Right, 0.4f);
+
+        c_centerSkin = skins[4].gameObject;
+        c_centerSkin.transform.DOLocalMove(position_C_Center, 0.4f);
+
+        l_leftSkin = skins[5].gameObject;
+        l_leftSkin.transform.DOLocalMove(position_L_Left, 0.4f);
+
+        //skins[3].gameObject.SetActive(false);
+        //skins[4].gameObject.SetActive(false);
+        //skins[5].gameObject.SetActive(false);
     }
     
     //Todo: Add the hiden ships on a place to make  arotation, like in the basball;
@@ -50,6 +74,16 @@ public class SkinChooser : MonoBehaviour
         {
             leftIndex = 5;
         }
+
+        if (rightIndex > 5)
+        {
+            rightIndex = 0;
+        }
+
+        safeIndexLeft = (leftIndex - 1 == -1) ? 5 : (leftIndex - 1);
+        l_leftSkin = skins[safeIndexLeft].gameObject;
+        l_leftSkin.transform.DOLocalMove(position_L_Left, 0.4f);
+
         leftSkin.transform.localPosition = positionLeft;
         leftSkin = skins[leftIndex].gameObject;
         leftSkin.transform.DOLocalMove(positionLeft,0.4f);
@@ -64,6 +98,11 @@ public class SkinChooser : MonoBehaviour
         rightSkin.transform.DOLocalMove(positionRight, 0.4f);
         rightSkin.transform.localScale /= 1.5f;
 
+        safeIndexRight = (rightIndex + 1 == 6) ? 5 : (rightIndex + 1);
+
+        safeIndexCenter = (safeIndexRight + 1 == 6) ? 0 : (safeIndexRight + 1);
+        c_centerSkin = skins[safeIndexCenter].gameObject;
+        c_centerSkin.transform.DOLocalMove(position_C_Center, 0.4f);
     }
 
     public void MoveQueueLeft()
@@ -77,21 +116,37 @@ public class SkinChooser : MonoBehaviour
             rightIndex = 0;
         }
 
-        leftSkin.gameObject.SetActive(false);
+
+        //Mestic liant molt i me matare
+        safeIndexCenter = (safeIndexCenter + 1 == 6) ? 0 : (safeIndexLeft + 1);
+        c_centerSkin = skins[safeIndexCenter].gameObject;
+        c_centerSkin.transform.DOLocalMove(position_C_Center, 0.4f);
+
+        safeIndexRight = (safeIndexRight +1== 6) ? 0 : (safeIndexRight + 1);
+        r_rightSkin = skins[safeIndexRight].gameObject;
+        r_rightSkin.transform.DOLocalMove(position_R_Right, 0.4f);
+
+        safeIndexLeft = (leftIndex - 1 == -1) ? 0 : (leftIndex + 1);
+        l_leftSkin = skins[safeIndexLeft].gameObject;
+        l_leftSkin.transform.DOLocalMove(position_L_Left, 0.4f);
+       
+        
+
+       
+
+
         leftSkin = skins[leftIndex].gameObject;
         leftSkin.transform.DOLocalMove(positionLeft,0.4f);
         leftSkin.transform.localScale /= 1.5f;
-
 
         centerSkin = skins[centerIndex].gameObject;
         centerSkin.transform.localScale *= 1.5f;
         centerSkin.transform.DOLocalMove(positionCenter, 0.4f);
 
-
         rightSkin = skins[rightIndex].gameObject;
         rightSkin.transform.DOLocalMove(positionRight, 0.4f);
-        rightSkin.gameObject.SetActive(true);
+        //rightSkin.gameObject.SetActive(true);
 
-
+    
     }
 }
