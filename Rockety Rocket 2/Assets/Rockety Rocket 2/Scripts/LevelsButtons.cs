54bd5ce4f7bf532ae1c25090e4be6433d245c041
MarcelSunyer <45765502+MarcelSunyer@users.noreply.github.com>
2025-07-27 20:37:58 +0200

    Test that dosnt work

---
+++ Rockety Rocket 2/Assets/Rockety Rocket 2/Scripts/LevelsButtons.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LevelsButtons : MonoBehaviour
+{
+    public GameObject father;
+
+    public Button[] levels = new Button[9];
+
+    public Sprite disabledStateSprite;
+
+    // Update is called once per frame
+    void Start()
+    {
+        for (int i = 1; i < levels.Length; i++)
+        {
+            SpriteState state = levels[i].spriteState;
+            state.disabledSprite = disabledStateSprite;
+            levels[i].spriteState = state;
+        }
+
+    }
+}
