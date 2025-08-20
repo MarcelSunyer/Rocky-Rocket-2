16e33d979416e08db948482e2395a3abb89ca4bf
MarcelSunyer <45765502+MarcelSunyer@users.noreply.github.com>
2025-08-20 20:46:46 +0200

    Optimization on level navegation & search

---
+++ Rockety Rocket 2/Assets/RocketyRocket2/Scripts/Ui/LevelManager.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+namespace RocketyRocket2
+{
+    public class LevelManager : MonoBehaviour
+    {
+        [SerializeField] private Button[] Levels;
+        [SerializeField] private int galaxy = 1;
+
+        private void Start()
+        {
+            for (int i = 0; i < Levels.Length; i++)
+            {
+                string levelName = Levels[i].name +"_"+ galaxy.ToString();
+                Levels[i].onClick.AddListener(()=> LoadLevel(levelName));
+            }
+        }
+
+        private void LoadLevel(string levelName)
+        {
+            SceneManager.LoadScene(levelName);
+        }
+    }
+}
