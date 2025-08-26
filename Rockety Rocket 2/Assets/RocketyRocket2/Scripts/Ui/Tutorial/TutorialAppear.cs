255f5299f2324486b4aa8c7a73e8f542a16557ab
MarcelSunyer <45765502+MarcelSunyer@users.noreply.github.com>
2025-08-26 17:12:05 +0200

    Tutorial almost done

---
+++ Rockety Rocket 2/Assets/RocketyRocket2/Scripts/Ui/Tutorial/TutorialAppear.cs
@@ -0,0 +1,41 @@
+﻿using DG.Tweening;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace RocketyRocket2
+{
+    public class TutorialAppear : MonoBehaviour
+    {
+
+        [SerializeField] private GameObject cam;
+
+        [SerializeField] private Button startGameplay;
+
+
+        void Start()
+        {
+           cam.GetComponent<CameraFollow>().enabled = false;
+           startGameplay.Select();
+           startGameplay.onClick.AddListener(StartGamePlay);
+        }
+
+        private void StartGamePlay()
+        {
+            StartCoroutine(HideTutorialAndMove());
+        }
+
+        private IEnumerator HideTutorialAndMove()
+        {
+            Debug.Log("ASd");
+            Tween tween = gameObject.transform.DOMoveY(-500,3);
+            tween.Play();
+            yield return tween.WaitForCompletion();
+
+            cam.GetComponent<CameraFollow>().enabled = true;
+        }
+
+    }
+}
